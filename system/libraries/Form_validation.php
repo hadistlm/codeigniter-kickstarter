3a983ea88562c5986b94e1db13dd9a96d689487d
Islaa <hadist.lm@gmail.com>
2019-08-09 14:04:03 +0700

    Fixing: error with form validation is_unique when using HMVC

--- system/libraries/Form_validation.php
+++ system/libraries/Form_validation.php
@@ -1118,7 +1118,7 @@ class CI_Form_validation {
 	public function is_unique($str, $field)
 	{
 		sscanf($field, '%[^.].%[^.]', $table, $field);
-		return isset($this->CI->db)
+		return is_object($this->CI->db)
 			? ($this->CI->db->limit(1)->get_where($table, array($field => $str))->num_rows() === 0)
 			: FALSE;
 	}
