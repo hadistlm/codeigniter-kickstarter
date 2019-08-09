92da91243a5d318565c50efb1400fcb7a162c776
Islaa <hadist.lm@gmail.com>
2019-08-09 14:15:18 +0700

    Adding: setting up environment

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+
+/* Detect Protocol */
+$protocol = (isset( $_SERVER['HTTPS'] ) AND $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
+
+$config['base_url'] = $protocol . '://localhost/CodeIgniter-HMVC/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +39,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -136,7 +140,7 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FALSE;
+$config['composer_autoload'] = 'vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -521,3 +525,14 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+
+/*
+|--------------------------------------------------------------------------
+| Module Location (H M.V.C.)
+|--------------------------------------------------------------------------
+*/
+
+$config['modules_locations'] = array( APPPATH.'modules/' => '../modules/');
+
+
